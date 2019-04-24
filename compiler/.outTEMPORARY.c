a88c5475603417a0723484ba7e242059785860ce
RandallZPorter <porterzane@gmail.com>
2019-04-24 10:56:26 -0500

    Changed file organization

--- compiler/.outTEMPORARY.c
+++ compiler/.outTEMPORARY.c
@@ -8,8 +8,12 @@ double a = 0.0;
 double b = 0.0;
 double c = 0.0;
 double d = 0.0;
+double e = 0.0;
+double f = 0.0;
+double g = 0.0;
+double h = 0.0;
+double i = 0.0;
 double x = 0.0;
-double y = 0.0;
 double con0 = 0.0;
 double con1 = 0.0;
 double printVar = 0.0;
@@ -161,11 +165,141 @@ void functionadd() {
 	pushD(c);
 }
 
-void functionloop() {
-while (1){
-	x = popD();
-	y = popD();
-	pushD(y);
+void functiondivisableTRUE() {
+	e = popD();
+	f = popD();
+	pushD(1);
+}
+
+void functiondivisableFALSE() {
+	e = popD();
+	f = popD();
+	pushD(0);
+}
+
+void functiondivisableLOOP() {
+	e = popD();
+	pushD(e);
+	functionsubOne();
+	e = popD();
+	pushD(e);
+	pushD(0);
+	pushD(e);
+	if (popD() > popD()){
+		functiondivisableLOOP();
+	}
+}
+
+void functiondivisableELSE() {
+	e = popD();
+	f = popD();
+	pushD(f);
+	pushD(e);
+	con0 = popD();
+	con1 = popD();
+	if (con0){
+		pushM(con0/con1);
+	} else {
+		con1 += (rand() % 2 == 0 ? 1 : -1);
+		pushM(con1);
+	}
+	pushD(popM());
+	functiondivisableLOOP();
+	e = popD();
+	pushD(e);
+	pushD(e);
+	pushD(0);
+	pushD(e);
+	if (popD() == popD()){
+		functiondivisableTRUE();
+	}
+	pushD(0);
+	pushD(e);
+	if (popD() > popD()){
+		functiondivisableFALSE();
+	}
+	pushD(0);
+	pushD(e);
+	if (popD() < popD()){
+		functiondivisableFALSE();
+	}
+}
+
+void functiondivisable() {
+	e = popD();
+	f = popD();
+	pushD(f);
+	pushD(e);
+	pushD(0);
+	pushD(f);
+	if (popD() == popD()){
+		functiondivisableTRUE();
+	}
+	pushD(1);
+	pushD(f);
+	if (popD() == popD()){
+		functiondivisableTRUE();
+	}
+	pushD(1);
+	pushD(f);
+	if (popD() > popD()){
+		functiondivisableELSE();
+	}
+}
+
+void functionprimeTRUE() {
+	pushD(1);
+}
+
+void functionprimeFALSE() {
+	pushD(0);
+}
+
+void functionprimeLOOP() {
+	g = popD();
+	h = popD();
+	pushD(h);
+	functionaddOne();
+	h = popD();
+	pushD(h);
+	pushD(g);
+	pushD(h);
+	pushD(g);
+	functiondivisable();
+	i = popD();
+	pushD(0);
+	pushD(i);
+	if (popD() == popD()){
+		functionprimeLOOP();
+	}
+}
+
+void functionprime() {
+	g = popD();
+	pushD(1);
+	pushD(g);
+	functionprimeLOOP();
+	g = popD();
+	h = popD();
+	pushD(h);
+	pushD(g);
+	if (popD() == popD()){
+		functionprimeTRUE();
+	}
+	pushD(h);
+	pushD(g);
+	if (popD() < popD()){
+		functionprimeFALSE();
+	}
+	pushD(h);
+	pushD(g);
+	if (popD() > popD()){
+		functionprimeFALSE();
+	}
+}
+
+void functionloopPRINT() {
+	pushD(x);
 	
 				printVar = popD();
 				if (printVar == (int)printVar){
@@ -176,10 +310,22 @@ while (1){
 				
 	printf("\n");
 	fflush(stdout);
+}
+
+void functionloop() {
+while (1){
+	x = popD();
 	pushD(x);
-	pushD(y);
+	functionprime();
+	pushD(1);
+	if (popD() == popD()){
+		functionloopPRINT();
+	}
 	pushD(x);
-	functionadd();
+	functionaddOne();
+	functionaddOne();
+	x = popD();
+	pushD(x);
 	}
 }
 
@@ -187,8 +333,18 @@ while (1){
 void main() {
 	stack = createStack(1000);
 	mathStack = createStack(1000);
-	pushD(0);
-	pushD(1);
+	pushD(2);
+	
+				printVar = popD();
+				if (printVar == (int)printVar){
+					printf("%d", (int)printVar);
+				} else {
+					printf("%lf", printVar);
+				}
+				
+	printf("\n");
+	fflush(stdout);
+	pushD(3);
 	functionloop();
 }
 
