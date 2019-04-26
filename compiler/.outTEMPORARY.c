282ead1861da20e64c6770649fa3a103e94c43b2
RandallZPorter <porterzane@gmail.com>
2019-04-25 21:42:56 -0500

    Built truth machine

--- compiler/.outTEMPORARY.c
+++ compiler/.outTEMPORARY.c
@@ -4,16 +4,6 @@
 #include <limits.h>
 
 //Variables
-double a = 0.0;
-double b = 0.0;
-double c = 0.0;
-double d = 0.0;
-double e = 0.0;
-double f = 0.0;
-double g = 0.0;
-double h = 0.0;
-double i = 0.0;
-double x = 0.0;
 double con0 = 0.0;
 double con1 = 0.0;
 double printVar = 0.0;
@@ -67,233 +57,9 @@ double popM() {
     return pop(mathStack);
 }
 
-void functiona() {
-do {
-	a = popD();
-	pushD(a);
-	pushD(0);
-	con0 = popD();
-	con1 = popD();
-	if (con0){
-		pushM(con0/con1);
-	} else {
-		con1 += (rand() % 2 == 0 ? 1 : -1);
-		pushM(con1);
-	}
-	pushD(popM());
-	b = popD();
-	pushD(a);
-	pushD(a);
-	pushD(b);
-	} while (popD() <= popD());
-	a = popD();
-	pushD(b);
-}
-
-void functionb() {
-do {
-	a = popD();
-	pushD(a);
-	pushD(0);
-	con0 = popD();
-	con1 = popD();
-	if (con0){
-		pushM(con0/con1);
-	} else {
-		con1 += (rand() % 2 == 0 ? 1 : -1);
-		pushM(con1);
-	}
-	pushD(popM());
-	b = popD();
-	pushD(a);
-	pushD(a);
-	pushD(b);
-	} while (popD() >= popD());
-	a = popD();
-	pushD(b);
-}
-
-void functionc() {
-do {
-	c = popD();
-	d = popD();
-	pushD(c);
-	functionb();
-	c = popD();
-	pushD(d);
-	functionb();
-	d = popD();
-	pushD(d);
-	pushD(c);
-	pushD(0);
-	pushD(d);
-	} while (popD() > popD());
-}
-
-void functiond() {
-	functionc();
-	c = popD();
-	d = popD();
-	pushD(c);
-}
-
-void functione() {
-do {
-	c = popD();
-	d = popD();
-	pushD(c);
-	functiona();
-	c = popD();
-	pushD(d);
-	functionb();
-	d = popD();
-	pushD(d);
-	pushD(c);
-	pushD(0);
-	pushD(d);
-	} while (popD() > popD());
-}
-
-void functionf() {
-	functione();
-	c = popD();
-	d = popD();
-	pushD(c);
-}
-
-void functiong() {
-	e = popD();
-	f = popD();
-	pushD(1);
-}
-
-void function9() {
-	e = popD();
-	f = popD();
-	pushD(0);
-}
-
-void function8() {
-do {
-	e = popD();
-	pushD(e);
-	functionb();
-	e = popD();
-	pushD(e);
-	pushD(0);
-	pushD(e);
-	} while (popD() > popD());
-}
-
-void function7() {
-	e = popD();
-	f = popD();
-	pushD(f);
-	pushD(e);
-	con0 = popD();
-	con1 = popD();
-	if (con0){
-		pushM(con0/con1);
-	} else {
-		con1 += (rand() % 2 == 0 ? 1 : -1);
-		pushM(con1);
-	}
-	pushD(popM());
-	function8();
-	e = popD();
-	pushD(e);
-	pushD(e);
-	pushD(0);
-	pushD(e);
-	if (popD() == popD()){
-		functiong();
-	}
-	pushD(0);
-	pushD(e);
-	if (popD() > popD()){
-		function9();
-	}
-	pushD(0);
-	pushD(e);
-	if (popD() < popD()){
-		function9();
-	}
-}
-
-void function6() {
-	e = popD();
-	f = popD();
-	pushD(f);
-	pushD(e);
-	pushD(0);
-	pushD(f);
-	if (popD() == popD()){
-		functiong();
-	}
-	pushD(1);
-	pushD(f);
-	if (popD() == popD()){
-		functiong();
-	}
-	pushD(1);
-	pushD(f);
-	if (popD() > popD()){
-		function7();
-	}
-}
-
-void function5() {
-	pushD(1);
-}
-
-void function4() {
-	pushD(0);
-}
-
-void function3() {
-do {
-	g = popD();
-	h = popD();
-	pushD(h);
-	functiona();
-	h = popD();
-	pushD(h);
-	pushD(g);
-	pushD(h);
-	pushD(g);
-	function6();
-	i = popD();
-	pushD(0);
-	pushD(i);
-	} while (popD() == popD());
-}
-
-void function2() {
-	g = popD();
-	pushD(1);
-	pushD(g);
-	function3();
-	g = popD();
-	h = popD();
-	pushD(h);
-	pushD(g);
-	if (popD() == popD()){
-		function5();
-	}
-	pushD(h);
-	pushD(g);
-	if (popD() < popD()){
-		function4();
-	}
-	pushD(h);
-	pushD(g);
-	if (popD() > popD()){
-		function4();
-	}
-}
-
 void function1() {
-	pushD(x);
+while (1){
+	pushD(1);
 	
 				printVar = popD();
 				if (printVar == (int)printVar){
@@ -304,22 +70,6 @@ void function1() {
 				
 	printf("\n");
 	fflush(stdout);
-}
-
-void function0() {
-while (1){
-	x = popD();
-	pushD(x);
-	function2();
-	pushD(1);
-	if (popD() == popD()){
-		function1();
-	}
-	pushD(x);
-	functiona();
-	functiona();
-	x = popD();
-	pushD(x);
 	}
 }
 
@@ -327,7 +77,12 @@ while (1){
 void main() {
 	stack = createStack(1000);
 	mathStack = createStack(1000);
-	pushD(2);
+	pushD(0);
+	pushD(1);
+	if (popD() == popD()){
+		function1();
+	}
+	pushD(0);
 	
 				printVar = popD();
 				if (printVar == (int)printVar){
@@ -338,8 +93,6 @@ void main() {
 				
 	printf("\n");
 	fflush(stdout);
-	pushD(3);
-	function0();
 }
 
 
